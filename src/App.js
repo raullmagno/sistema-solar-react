add78ccadc3778f9311a8b3119953147fe93ab46
Raul Magno <raulmagno23@gmail.com>
2022-10-21 20:43:19 -0400

    Requisito 10 - Renderiza lista de missões espaciais

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
+import PlanetCard from './components/PlanetCard';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <p>Sistema Solar</p>
         <Header />
         <SolarSystem />
+        <PlanetCard />
         <Missions />
       </div>
     );
--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
+    const missões = missions;
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missões.map((missão) => (<MissionCard
+          name={ missão.name }
+          year={ missão.year }
+          country={ missão.country }
+          destination={ missão.destination }
+          key={ missão.name }
+        />))}
+        ;
       </div>
 
     );
