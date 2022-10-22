45cf226177784864fc25515f05715cdba79f7751
Raul Magno <raulmagno23@gmail.com>
2022-10-21 20:02:44 -0400

    Requisito 6 - Renderiza uma lista com planetas do Sistema Solar

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Title from './Title';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
+    const planetas = Planets;
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planetas.map((item) => (<PlanetCard
+          planetName={ item.name }
+          planetImage={ item.image }
+          key={ item.name }
+        />))}
       </div>
 
     );
